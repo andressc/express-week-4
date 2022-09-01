79b8ed21c7cb9b1409c83f4337e293634921bf2f
andrejrebrov <674511674511aaqqww>
2022-09-01 21:31:24 +0300

    lesson 5 fix2

--- src/domain/remote-user-ip-service.ts
+++ src/domain/remote-user-ip-service.ts
@@ -1,16 +1,13 @@
 import { idCreator } from '../helpers/idCreator';
 import { RemoteUserIpType } from '../types/remoteUserIpType';
 import { remoteUserIpRepository } from '../repositories/remote-user-ip-repository';
-import add from 'date-fns/add';
 
 export const remoteUserIpService = {
 	async createRemoteUserIp(ip: string): Promise<RemoteUserIpType | null> {
 		return await remoteUserIpRepository.createRemoteUserIp({
 			id: idCreator(),
 			ip,
-			date: add(new Date(), {
-				seconds: 10,
-			}),
+			date: new Date(),
 		});
 	},
 
--- src/repositories/remote-user-ip-repository.ts
+++ src/repositories/remote-user-ip-repository.ts
@@ -1,5 +1,6 @@
 import {remoteUserIpCollection} from '../db/db';
 import {RemoteUserIpType} from '../types/remoteUserIpType';
+import sub from 'date-fns/sub';
 
 export const remoteUserIpRepository = {
 	async createRemoteUserIp(remoteUserIp: RemoteUserIpType): Promise<RemoteUserIpType> {
@@ -11,6 +12,6 @@ export const remoteUserIpRepository = {
 	},
 
 	async countRemoteUserIp(ip: string): Promise<number> {
-		return await remoteUserIpCollection.count({date: {$gt: new Date()}, ip});
+		return await remoteUserIpCollection.count({date: {$gt: sub(new Date(), {seconds: 10})}, ip});
 	},
 };
