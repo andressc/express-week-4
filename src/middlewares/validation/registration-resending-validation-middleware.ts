df24fd4b6ade9d52b6fbe4e44f9f35b28528946c
andrejrebrov <674511674511aaqqww>
2022-09-16 21:00:24 +0300

    Refactoring for ioc

--- src/middlewares/validation/registration-resending-validation-middleware.ts
+++ src/middlewares/validation/registration-resending-validation-middleware.ts
@@ -7,8 +7,8 @@ const usersService = container.resolve(UsersService);
 
 export const registrationResendingValidationMiddleware = [
 	body('email').custom(async (value) => {
-		const user: UsersTypeDb = await usersService.findUserByEmail(value);
-		//if (!user) throw new Error('user not found');
+		const user: UsersTypeDb | null = await usersService.findUserByEmail(value);
+		if (!user) throw new Error('user not found');
 		if (user.emailConfirmation.isConfirmed) throw new Error('already confirmed');
 		if (user.emailConfirmation.expirationDate < new Date())
 			throw new Error('confirmed code date expired');
